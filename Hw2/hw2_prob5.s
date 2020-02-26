3f4d99f3e6fcf49ab19c2e9c09ba5fdadf914191
aviaryal <avinash.aryal@mavs.uta.edu>
2020-02-25 20:45:18 -0600

    function

---
+++ Hw2/hw2_prob5.s
@@ -0,0 +1,16 @@
+/*Avinash Aryal 1001727418*/
+.global add32
+.global sub64
+.global minU16
+.global minS32
+.global isLessThanU16
+.global isLessThanS16
+.global shiftLeftU16
+.global shiftS8
+.global 
+.global
+.global
+.global
+.global
+.global
+.global
