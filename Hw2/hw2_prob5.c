9f51b0fa99ffc18ba1b9e018e0cfc2404bdd99c8
aviryl <avinash.aryal@mavs.uta.edu>
2020-02-27 12:16:01 -0600

    Hw

--- Hw2/hw2_prob5.c
+++ Hw2/hw2_prob5.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 #include <stdint.h>
 extern uint64_t add32(uint32_t x, uint32_t y); // returns x+y
-//extern uint64_t sub64(uint64_t x, uint64_t y); // returns (x-y)%(2 64 )
-//extern uint16_t minU16(uint16_t x, uint16_t y); // returns the minimum of x, y
-//extern int32_t minS32(int32_t x, int32_t y); // returns the minimum of x, y
+extern uint64_t sub64S(uint64_t x, uint64_t y); // returns (x-y)%(2 64 )
+extern uint16_t minU16(uint16_t x, uint16_t y); // returns the minimum of x, y
+extern int32_t minS32(int32_t x, int32_t y); // returns the minimum of x, y
 //extern bool isLessThanU16(uint16_t x, uint16_t y); // returns 1 if x<y, 0 else
 //extern bool isLessThanS16(int16_t x, int16_t y); // returns 1 if x<y, 0 else
 //extern uint16_t shiftLeftU16 (uint16_t x, uint16_t p); // returns x << p =((x*2^(p)) % 2 32 ) for p = 0..31
@@ -15,11 +15,37 @@ extern uint64_t add32(uint32_t x, uint32_t y); // returns x+y
 //extern bool isStrEqual(const char* str1, const char* str2); // returns 1 the strings match, 0 otherwise
 //extern void strCopy(char* strTo, const char* strFrom); // copies strFrom to strTo
 
+//disassemble
 
 int main()
 {
-  uint32_t a=4;
+  /*
+  uint32_t a=4294967295;
   uint32_t b=5;
   uint64_t c;
   c=add32(a,b);
+  printf("THIS IS ADD32 RETURN 64 bit %llu \n",c);
+  
+  /*
+  uint64_t a=6294967299;
+  uint64_t b=1;
+  uint64_t c;
+  c=sub64S(a,b);
+  printf("%llu \n",c);
+  */
+  
+  /*
+  uint16_t a=3;
+  uint16_t b=5;
+  uint16_t c;
+  c=minU16(a,b);
+  printf("The vakue which is greater between a= %u and b= %u is: %u \n",a,b,c);
+  
+  */
+  int32_t a=500;
+  int32_t b=600;
+  int32_t c;
+  c=minS32(a,b);
+  printf("%d \n",c);
+  
 }
--- Hw2/hw2_prob5.s
+++ Hw2/hw2_prob5.s
@@ -1,9 +1,12 @@
 /*Avinash Aryal 1001727418*/
 .global add32
+.global sub64S
+.global minU16
+.global minS32
 /*
-.global sub64
-//.global minU16
-//.global minS32
+
+//
+//
 //.global isLessThanU16
 //.global isLessThanS16
 //.global shiftLeftU16
@@ -15,9 +18,31 @@
 //.global strCopy
 */
 
+//disassemble
 
 .text
 
 add32:
-  ADD R0,R0,R1
+  ADDS R0,R0,R1
+  MOV R1,#0
+  ADC R1,R1,#0    
   BX LR
+
+sub64S:
+	SUBS R0,R0,R2
+	SBC R1,R1,R3
+	BX LR
+
+minU16:
+	SUBS R2,R0,R1
+	MOVGT R0,R0   //condition Greater than (GT) Z clear, N=V (N and V set or N and V clear)
+	MOVLE R0,R1   //condition less than or equal (LE) Z set or N<>V (N set and V clear) or (N clear and V set)
+	BX LR
+minS32:
+	MOVGT R0,R0   //condition Greater than (GT) Z clear, N=V (N and V set or N and V clear)
+	MOVLE R0,R1   //condition less than or equal (LE) Z set or N<>V (N set and V clear) or (N clear and V set)
+	BX LR
+	
+	
+
+
