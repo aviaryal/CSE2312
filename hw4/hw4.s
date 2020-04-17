3bcf393711f6dc21120a3645936e5253d7035751
aviryl <avinash.aryal@mavs.uta.edu>
2020-04-16 21:09:18 -0500

    working

--- hw4/hw4.s
+++ hw4/hw4.s
@@ -10,17 +10,14 @@ diffF32:
 calculate_diff:
 	CMP R3,#0
 	BEQ diff32_end
-	VLDR S1,[R1,R4]
-	VLDR S2,[R2,R4]
+	VLDR S1,[R1]
+	VLDR S2,[R2]
 	VSUB.F32 S0,S1,S2
-	VSTR S0,[R0,R4]
+	VSTR S0,[R0]
 	ADD R4,R4,#4
-	/*	
 	ADD R1,R1,#4
 	ADD R2,R2,#4	
-	ADD R0,R0,#4
-	*/
-	
+	ADD R0,R0,#4	
 	SUB R3,R3,#1
 	B calculate_diff
 diff32_end:
